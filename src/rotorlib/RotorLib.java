a986c5b9d28c1f5bf3165304f7b71c5cecc7b534
uujuju <84468589+uujuju1@users.noreply.github.com>
2021-11-28 20:50:41 -0300

    Update RotorLib.java

--- src/rotorlib/RotorLib.java
+++ src/rotorlib/RotorLib.java
@@ -16,6 +16,13 @@ public class RotorLib extends Mod {
 
 	@Override
 	public void loadContent() {
-		new LibCopter().load();
+		Events.on(ClientLoadEvent.class, e -> {
+        		Time.runTask(10f, () -> {
+                		BaseDialog dialog = new BaseDialog("frog");
+        	        	dialog.cont.add("This mod has an example. If you dont want the example use the other jar file.").row();
+                		dialog.cont.button("Hmmm", dialog::hide).size(100f, 50f);
+                		dialog.show();
+        		});
+        	});
 	}
 }
