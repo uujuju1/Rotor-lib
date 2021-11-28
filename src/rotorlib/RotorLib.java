612bcbf974ebeca277e14f7bbd4db30c2ac11f0c
uujuju <84468589+uujuju1@users.noreply.github.com>
2021-11-28 20:53:05 -0300

    Update RotorLib.java

--- src/rotorlib/RotorLib.java
+++ src/rotorlib/RotorLib.java
@@ -13,17 +13,6 @@ import mindustry.ui.dialogs.*;
 import rotorlib.content.*;
 
 public class RotorLib extends Mod {
-	
-	public RotorLib() {
-		Events.on(ClientLoadEvent.class, e -> {
-        		Time.runTask(10f, () -> {
-                		BaseDialog dialog = new BaseDialog("frog");
-        	        	dialog.cont.add("This mod dont have an example. If you dont want the example use the other jar file.").row();
-                		dialog.cont.button("Hmmm", dialog::hide).size(100f, 50f);
-                		dialog.show();
-        		});
-        	});
-	}
 	@Override
 	public void loadContent() {
 		Log.info("no example to load");
